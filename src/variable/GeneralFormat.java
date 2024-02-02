24a47bbfc84d4488ad75bd633ca2ed753b795bb1
shreySaxena <shreyyerhs07@gmail.com>
2024-02-02 13:41:07 +0530

    added format for both type of problems

---
+++ src/variable/GeneralFormat.java
@@ -0,0 +1,36 @@
+package variable;
+
+public class GeneralFormat {
+    //this is general format for variable size window problems and i am follwing it.
+//    public static int func(int arr[]){
+//
+//        int start=0;
+//        int end=0;
+//
+//        while(end<arr.length){
+//
+//            //calculations
+//
+//            if(condition<k){end++;}
+//
+//            else if(condition==k){
+//                //this is possible answer so update the answer
+//                end++;
+//            }
+//            else{
+//                while(condition>k){
+//                    remove calculations for start
+//                    start++;
+//                }
+//                end++;
+//            }
+//        }
+//        return ans;
+//
+//    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
